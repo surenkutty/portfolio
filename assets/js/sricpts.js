82c0e4b20631a4443963a7862ad9b27a21fd7016
surenkutty <105663626+surenkutty@users.noreply.github.com>
2024-08-09 09:58:18 -0700

    first commit

---
+++ assets/js/sricpts.js
@@ -0,0 +1,37 @@
+function myMenuFunction(){
+    var menuBtn = document.getElementById("myNavMenu");
+    if(menuBtn.className === "nav-menu"){
+      menuBtn.className += " responsive";
+    } else {
+      menuBtn.className = "nav-menu";
+    }
+  }
+
+  window.onscroll = function() {headerShadow()};
+  function headerShadow() {
+    const navHeader =document.getElementById("header");
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop >  50) {
+      navHeader.style.boxShadow = "0 1px 6px rgba(0, 0, 0, 0.1)";
+      navHeader.style.height = "70px";
+      navHeader.style.lineHeight = "70px";
+    } else {
+      navHeader.style.boxShadow = "none";
+      navHeader.style.height = "90px";
+      navHeader.style.lineHeight = "90px";
+    }
+  }
+
+  var typingEffect = new Typed(".typedText",{
+    strings : ["Designer","Fullstack Developer","AppDeveloper"],
+    loop : true,
+    typeSpeed : 100, 
+    backSpeed : 80,
+    backDelay : 2000
+ })
+
+ sr.reveal('.featured-text-card',{})
+ sr.reveal('.featured-name',{delay: 100})
+ sr.reveal('.featured-text-info',{delay: 200})
+ sr.reveal('.featured-text-btn',{delay: 200})
+ sr.reveal('.social_icons',{delay: 200})
+ sr.reveal('.featured-image',{delay: 300})
