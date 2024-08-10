48c197cb898bce3cc05113a036c03360fcaffa82
surenkutty <105663626+surenkutty@users.noreply.github.com>
2024-08-10 09:51:45 -0700

    servicepageedit

--- assets/js/sricpts.js
+++ assets/js/sricpts.js
@@ -29,9 +29,26 @@ function myMenuFunction(){
     backDelay : 2000
  })
 
- sr.reveal('.featured-text-card',{})
- sr.reveal('.featured-name',{delay: 100})
- sr.reveal('.featured-text-info',{delay: 200})
- sr.reveal('.featured-text-btn',{delay: 200})
- sr.reveal('.social_icons',{delay: 200})
- sr.reveal('.featured-image',{delay: 300})
+ const sr = ScrollReveal({
+  origin: 'top',
+  distance: '80px',
+  duration: 2000,
+  reset: true     
+})
+/* -- HOME -- */
+sr.reveal('.featured-text-card',{})
+sr.reveal('.featured-name',{delay: 100})
+sr.reveal('.featured-text-info',{delay: 200})
+sr.reveal('.featured-text-btn',{delay: 200})
+sr.reveal('.social_icons',{delay: 200})
+sr.reveal('.featured-image',{delay: 300})
+
+const srleft = ScrollReveal({
+  origin: 'left',
+  distance: '80px',
+  duration: 2000,
+  reset: true     
+})
+
+srleft.reveal('.s2-title',{})
+srleft.reveal('.card',{delay:100})
